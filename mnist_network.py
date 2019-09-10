10917fe49f3fcc21ba35b44f419364f926f5a0e8
choi <lnatsagdor-choijilsuren@dentsu.co.jp>
2019-09-10 18:04:54 +0900

    almost done

---
+++ mnist_network.py
@@ -0,0 +1,59 @@
+'''
+SCAN network on mnist 
+Choijilsuren
+
+'''
+
+
+import torch.nn as nn
+from networks import PrimeCapsuleLayer, DigitCapsuleLayer, ConvLayer, NonLocalLayer
+from torch.optim import Adam
+
+class MnistSCAN(nn.Module):
+
+    def __init__(self, convlayer, primary_cap, digitcap):
+
+        super(MnistSCAN, self).__init__()
+
+        self.convlayer = ConvLayer()
+
+        self.non_local = NonLocalLayer()
+        self.prime_layer = PrimeCapsuleLayer()
+
+        self.digit_layer = DigitCapsuleLayer()
+        
+        
+    def forward(self, x):
+
+        conv_out = self.convlayer(x)
+        nonlocal_out = self.non_local(conv_out)
+
+        prime_out = self.prime_layer(nonlocal_out)
+        digit_out = self.digit_layer(prime_out)
+
+        reconst = self.recont_layer(digit_out)
+
+        return reconst, digit_out
+        
+
+    def loss(self, ):
+
+        pass
+
+    
+    def margin_loss(self,):
+
+        pass
+
+
+    def reconst_loss(self, ):
+
+        pass
+
+    
+
+if __name__ == '__main__':
+
+    pass
+
+
