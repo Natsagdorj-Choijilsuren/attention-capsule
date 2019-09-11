b414499d9c00ce59ccc87fc0e498b49b46ba04a8
choi <lnatsagdor-choijilsuren@dentsu.co.jp>
2019-09-11 11:37:36 +0900

    firts version of networks

--- networks.py
+++ networks.py
@@ -12,6 +12,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 
 
+USE_CUDA = True if torch.cuda.is_available() else False
 
 class PrimeCapsuleLayer(nn.Module):
 
@@ -63,11 +64,33 @@ class DigitCapsuleLayer(nn.Module):
         squared_norm = (input_tensor ** 2).sum(-1, keepdim = True)
         output_tensor = squared_norm * input_tensor / ((1. + squared_norm)*torch.sqrt(squared_norm))
         return output_tensor
-        
+
+    
     def forward(self, x):
 
         batch_size = x.size(0)
+        x = torch.stack([x] * self.num_capsules, dim=2).unsqueeze(4)
 
+        W = torch.cat([self.W] * batch_size, dim=0)
+        u_hat = torch.matmul(W, x)
+
+        b_ij = Variable(torch.zeros(1, self.num_routes, self.num_capsules, 1))
+        if USE_CUDA:
+            b_ij = b_ij.cuda()
+
+        num_iterations = 1 
+        for iteration in range(num_iterations):
+            c_ij = F.softmax(b_ij, dim=1)
+            c_ij = torch.cat([c_ij] * batch_size, dim=0).unsqueeze(4)
+
+            s_j = (c_ij * u_hat).sum(dim=1, keepdim=True)
+            v_j = self.squash(s_j)
+
+            if iteration < num_iterations - 1:
+                a_ij = torch.matmul(u_hat.transpose(3, 4), torch.cat([v_j] * self.num_routes, dim=1))
+                b_ij = b_ij + a_ij.squeeze(4).mean(dim=0, keepdim=True)
+
+        return v_j.squeeze(1)
         
 
 
@@ -147,8 +170,22 @@ class Reconstruction(nn.Module):
             
     def forward(self, x, data):
 
-        pass
+        classes = torch.sqrt((x**2).sum(2))
+        classes = F.softmax(classes, dim=0)
 
+        _, max_length_indices = classes.max(dim=1)
+        masked = Variable(torch.sparse.torch.eye(10))
+
+        if USE_CUDA:
+            masked = masked.cuda()
+
+            
+        masked = masked.index_select(dim=0, index=Variable(max_length_indices.squeeze(1).data))
+        t = (x * masked[:, :, None, None]).view(x.size(0), -1)
+        reconstructions = self.reconstraction_layers(t)
+        reconstructions = reconstructions.view(-1, self.input_channel, self.input_width, self.input_height)
+        return reconstructions, masked
+        
 
     
 
