55d6144470fc4836c7c6a833cfe1c2707273e631
choi <lnatsagdor-choijilsuren@dentsu.co.jp>
2019-09-28 01:20:59 +0900

    added loss

--- mnist_network.py
+++ mnist_network.py
@@ -12,17 +12,23 @@ from torch.optim import Adam
 
 class MnistSCAN(nn.Module):
 
-    def __init__(self, convlayer, primary_cap, digitcap):
+    def __init__(self):
 
         super(MnistSCAN, self).__init__()
 
-        self.convlayer = ConvLayer()
+        self.convlayer = ConvLayer(input_channel=1, out_channels=256,
+                                   kernel_size=9, stride=1)
 
-        self.non_local = NonLocalLayer()
-        self.prime_layer = PrimeCapsuleLayer()
-
-        self.digit_layer = DigitCapsuleLayer()
+        self.non_local = NonLocalLayer(in_channel=256, inter_channel=128,
+                                       out_channel=256)
         
+        self.prime_layer = PrimeCapsuleLayer(caps_dim=8, in_channel=256,
+                                             out_channel=32, num_routes=32*6*6,
+                                             kernel_size=9)
+        
+        self.digit_layer = DigitCapsuleLayer(num_capsule=10, num_routes=32*6*6,
+                                             incap_dim=8, outcap_dim=16)
+                
         
     def forward(self, x):
 
@@ -37,25 +43,38 @@ class MnistSCAN(nn.Module):
         return reconst, digit_out
         
     
-    def loss(self, ):
-
-        pass
+   def loss(self, data, x, target, reconstructions):
+        return self.margin_loss(x, target) + self.reconstruction_loss(data, reconstructions)     
 
     
     #margin loss 
-    def margin_loss(self, data, x,):
+    def margin_loss(self, labels, x):
 
-        pass 
+        batch_size = x.size(0)
+
+        v_c = torch.sqrt((x ** 2).sum(dim=2, keepdim=True))
+
+        left = F.relu(0.9 - v_c).view(batch_size, -1)
+        right = F.relu(v_c - 0.1).view(batch_size, -1)
+
+        loss = labels * left + 0.5 * (1.0 - labels) * right
+        loss = loss.sum(dim=1).mean()
+
+        return loss
 
 
     def reconst_loss(self, ):
 
-        pass
+         loss = self.mse_loss(reconstructions.view(reconstructions.size(0), -1),
+                              data.view(reconstructions.size(0), -1))
+         
+        return loss * 0.0005
 
     
 
 if __name__ == '__main__':
 
-    pass
+    mnist_network = MnistSCAN()
 
+    
 
